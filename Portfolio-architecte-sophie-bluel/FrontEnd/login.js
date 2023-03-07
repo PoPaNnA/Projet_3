4b1c86bf198ed894ad3c3db9f85473f6b2c3c5b7
PoPaNnA <yatchina@gmail.com>
2023-03-07 14:37:38 +0100

    Creation la page de login

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/login.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+let myForm = document.getElementById("myForm");
+let maPassword = document.getElementById("password");
+
+document.querySelector("submit").onclick = function(){
+    alert("Vous avec clique");
+}
+
+/*myForm.addEventListener("submit", function(e)
+    /*let myEmail = document.getElementById("email");
+    if(myEmail.ariaValueMax.trim() = ""){
+        letmyError = document.getElementById("error");
+        myError.innerHTML = "Le champ e-mail est requis";
+        myError.style.color = "red";
+        e.preventDefault();
+    }
+})*/
